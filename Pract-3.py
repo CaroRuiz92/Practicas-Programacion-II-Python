f5387e412d2aadf2e0fff87615a40d7f769171fb
Irma Carolina Ruiz <icruiz92@outlook.com>
2023-01-21 20:07:28 -0300

    Práctica 3 - Algoritmos de Ordenamiento (básicos)

--- Pract-3.py
+++ Pract-3.py
@@ -1,18 +1,37 @@
 # ALGORITMOS DE ORDENAMIENTO
 
+# Ordenamiento por selección
+
 def ordenar_por_seleccion(A):
     long = len(A)
     minimo = None
     posicion = None
-    for item1 in range(long-1):
+    for item1 in range(long-1):   # todos serán tomados como el mínimo
         minimo = A[item1]
         posicion = item1
-        for item2 in range(item1, long):
+        for item2 in range(item1, long):   # desde el item seleccionado hasta el final
             if A[item2] < minimo:
                 minimo = A[item2]
                 posicion = item2
         A[posicion], A[item1] = A[item1], A[posicion]
     return str(A)
 
-lista = [5,8,1,3]
+def ordenar_por_insercion(A):
+    long = len(A)
+    for pos in range(1,long):
+        item = A[pos]
+        anterior = pos-1
+        while anterior >= 0:
+            if item < A[anterior]:
+                A[anterior+1] = A[anterior]
+                anterior = anterior-1
+            else:
+                break
+        A[anterior+1] = item
+    return str(A)
+
+
+
+lista = [5,2,6,1]
 print(ordenar_por_seleccion(lista))
+print(ordenar_por_insercion(lista))

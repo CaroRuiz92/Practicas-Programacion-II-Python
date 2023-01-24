cc4fe5786d31abb744c33a79ca2ed84dac737a69
Irma Carolina Ruiz <icruiz92@outlook.com>
2023-01-24 16:21:13 -0300

    Práctica 3 de Programación II (código básico y dos actividades)

--- Pract-3.py
+++ Pract-3.py
@@ -1,4 +1,6 @@
 # ALGORITMOS DE ORDENAMIENTO
+import random
+
 
 # Ordenamiento por selección
 
@@ -33,6 +35,7 @@ def ordenar_por_insercion(A):
     return str(A)
 
 # Quicksort
+
 def pivote(A):
     return A[0]   # elección arbitraria
 def quicksort(A):
@@ -44,9 +47,92 @@ def quicksort(A):
     may_ord = quicksort(mayores)
 
     return men_ord + [pivote] + may_ord
-
-
+"""
 lista = [5,3,1,7]
 print(ordenar_por_seleccion(lista))
 print(ordenar_por_insercion(lista))
 print(quicksort(lista))
+"""
+
+#                   Ejercicios
+
+# **Ejercicio 1**: Implementar una cola de prioridad que internamente mantenga
+# los elementos ordenados utilizando ordenamiento por inserción.
+
+class PriorityQueue:
+    def __init__(self):
+        self.items = []
+    def __str__(self):
+        return str(self.items)
+    def insert(self, elem):
+        self.items.append(elem)
+        long = len(self.items)
+        for pos in range(1, long):
+            item = self.items[pos]
+            anterior = pos - 1
+            while anterior >= 0:
+                if item < self.items[anterior]:
+                    self.items[anterior + 1] = self.items[anterior]
+                    anterior = anterior - 1
+                else:
+                    break
+            self.items[anterior + 1] = item
+        return str(self.items)
+
+    def remove(self):
+        self.items.pop(0)
+    def isEmpty(self):
+        return self.items == []
+
+
+prueba = PriorityQueue()
+prueba.insert(2)
+prueba.insert(1)
+prueba.insert(5)
+prueba.insert(4)
+print(prueba)
+
+
+# **Ejercicio 2**: Escribir una función que verifique eficientemente si una lista está ordenada,
+# utilícela para implementar _bogosort_:
+# Utilice bogosort para ordenar [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] y observe el tiempo que le toma.
+
+# Versión 1:
+def verificar_orden1(lista):
+    if len(lista) < 2:
+        return "Ordenada de forma trivial"
+    for num in range(len(lista)-1):
+        if lista[num] == num+1:
+            return "Está ordenada"
+        else:
+            return "No está ordenada"
+
+
+def verificar_orden2(lista):
+    if len(lista) < 2:
+        return True
+    for i in range(len(lista) - 1):
+        if lista[i] > lista[i + 1]:
+            return False
+    return True
+
+
+def bogosort(lista):
+    from random import shuffle
+    while not verificar_orden2(lista):
+        random.shuffle(lista)
+    return lista
+
+ejemplo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ejemplo1 = [5, 6, 7, 8, 9, 10, 1, 2, 3, 4]
+print(verificar_orden1(ejemplo))
+print(bogosort(ejemplo1))
+
+
+# **Ejercicio 3**: Se les da una lista de números enteros. Determinar la cantidad de elementos únicos, o sea,
+# la cantidad de elementos ignorando los repetidos.
+# **Nota**: Utilice ordenamiento para resolver.
+# [1, 5, 3, 3, 5, 1]
+# # Salida: 3
+# Resolver de forma eficiente: que sea capaz de computar la solución
+# para una lista de 10000 elementos en aproximadamente un segundo.
